4543188f326e499414c6ed4b0140ef1b2f285171
Lance Hutchinson <lvhutchinson@gmail.com>
2014-01-21 16:37:58 -0700

    Updating model to hide ID fields

--- inventory/admin.py
+++ inventory/admin.py
@@ -6,8 +6,14 @@ admin.site.register(FinishedGoodsInventory)
 admin.site.register(FinishedGoodsInventoryItem)
 admin.site.register(FinishedGoodsInventoryType)
 admin.site.register(FinishedGoodsMaterialsUsed)
-admin.site.register(MaterialsInventory)
-admin.site.register(MaterialsInventoryItem)
+class MaterialsInventoryAdmin(admin.ModelAdmin):
+    fields = ['item', 'quantity', 'transaction', 'cost' ]
+    readonly_fields = ['cost']
+    list_display = ('item', 'quantity', 'cost')
+admin.site.register(MaterialsInventory, MaterialsInventoryAdmin)
+class MaterialsInventoryItemAdmin(admin.ModelAdmin):
+    fields = ['name', 'unit', 'price_per_unit', 'description', 'photo', 'date_last_purchased', 'date_last_used']
+admin.site.register(MaterialsInventoryItem, MaterialsInventoryItemAdmin)
 admin.site.register(Role)
 admin.site.register(Transaction)
 admin.site.register(Unit)
--- inventory/models.py
+++ inventory/models.py
@@ -11,86 +11,102 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Account(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.IntegerField()
+    name = models.CharField(max_length=128L)
     class Meta:
         db_table = 'account'
+    def __unicode__(self):
+        return self.name
 
 class FinishedGoodsInventory(models.Model):
-    id = models.IntegerField(primary_key=True)
-    finishedgoodsinventoryitemid = models.ForeignKey('FinishedGoodsInventoryItem', db_column='finishedGoodsInventoryItemId') # Field name made lowercase.
-    quantity = models.IntegerField()
+    item = models.ForeignKey('FinishedGoodsInventoryItem', db_column='finishedGoodsInventoryItemId') # Field name made lowercase.
+    quantity = models.IntegerField() 
     class Meta:
         db_table = 'finishedGoodsInventory'
+    def __unicode__(self):
+        return self.item.description + " quantity " + str(self.quantity)
+	
+	
 
 class FinishedGoodsInventoryItem(models.Model):
-    id = models.IntegerField(primary_key=True)
-    typeid = models.ForeignKey('FinishedGoodsInventoryType', db_column='typeId') # Field name made lowercase.
+    type = models.ForeignKey('FinishedGoodsInventoryType', db_column='typeId') # Field name made lowercase.
     description = models.TextField()
     photo = models.TextField()
     class Meta:
         db_table = 'finishedGoodsInventoryItem'
+    def __unicode__(self):
+        return self.description
 
 class FinishedGoodsInventoryType(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=128L)
     description = models.TextField()
     class Meta:
         db_table = 'finishedGoodsInventoryType'
+    def __unicode__(self):
+        return self.name
 
 class FinishedGoodsMaterialsUsed(models.Model):
-    id = models.IntegerField(primary_key=True)
-    finishedgoodsinventoryitemid = models.ForeignKey(FinishedGoodsInventoryItem, db_column='finishedGoodsInventoryItemId') # Field name made lowercase.
-    materialsinventoryitemid = models.ForeignKey('MaterialsInventoryItem', db_column='materialsInventoryItemId') # Field name made lowercase.
-    quantityused = models.FloatField(db_column='quantityUsed') # Field name made lowercase.
+    item = models.ForeignKey(FinishedGoodsInventoryItem, db_column='finishedGoodsInventoryItemId') # Field name made lowercase.
+    material_used = models.ForeignKey('MaterialsInventoryItem', db_column='materialsInventoryItemId') # Field name made lowercase.
+    quantity = models.FloatField(db_column='quantityUsed') # Field name made lowercase.
     class Meta:
         db_table = 'finishedGoodsMaterialsUsed'
+    def __unicode__(self):
+        return str(self.item.id) + " " + self.item.description
 
 class MaterialsInventory(models.Model):
-    id = models.IntegerField(primary_key=True)
-    materialsinventoryitemid = models.ForeignKey('MaterialsInventoryItem', db_column='materialsInventoryItemId') # Field name made lowercase.
+    item = models.ForeignKey('MaterialsInventoryItem', db_column='materialsInventoryItemId') # Field name made lowercase.
     quantity = models.FloatField()
+    transaction = models.ForeignKey('Transaction', db_column='transactionId') # Field name made lowercase.
     class Meta:
         db_table = 'materialsInventory'
+    def __unicode__(self):
+        return self.item.name + " quantity of " + str(self.quantity)
+    def cost(self):
+	return self.quantity * self.item.price_per_unit
 
 class MaterialsInventoryItem(models.Model):
-    id = models.IntegerField(primary_key=True)
-    unitid = models.ForeignKey('Unit', db_column='unitId') # Field name made lowercase.
-    priceperunit = models.FloatField(db_column='pricePerUnit') # Field name made lowercase.
+    unit = models.ForeignKey('Unit', db_column='unitId') # Field name made lowercase.
+    price_per_unit = models.FloatField(db_column='pricePerUnit') # Field name made lowercase.
     description = models.TextField()
     photo = models.TextField()
-    datelastpurchased = models.DateField(db_column='dateLastPurchased') # Field name made lowercase.
-    datelastused = models.DateField(db_column='dateLastUsed') # Field name made lowercase.
+    date_last_purchased = models.DateField(db_column='dateLastPurchased') # Field name made lowercase.
+    date_last_used = models.DateField(db_column='dateLastUsed') # Field name made lowercase.
     name = models.CharField(max_length=256L)
     class Meta:
         db_table = 'materialsInventoryItem'
+    def __unicode__(self):
+        return self.name
 
 class Role(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=256L)
     class Meta:
         db_table = 'role'
+    def __unicode__(self):
+        return self.name
 
 class Transaction(models.Model):
-    id = models.IntegerField(primary_key=True)
     date = models.DateField()
     description = models.TextField()
     amount = models.FloatField()
-    accountid = models.ForeignKey(Account, db_column='accountId') # Field name made lowercase.
+    account = models.ForeignKey(Account, db_column='accountId') # Field name made lowercase.
     class Meta:
         db_table = 'transaction'
+    def __unicode__(self):
+        return str(self.date) + " " +" " +  self.description
 
 class Unit(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=32L)
     class Meta:
         db_table = 'unit'
+    def __unicode__(self):
+        return self.name
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=128L, db_column='userName') # Field name made lowercase.
     displayname = models.CharField(max_length=128L, db_column='displayName') # Field name made lowercase.
     roleid = models.ForeignKey(Role, db_column='roleId') # Field name made lowercase.
     class Meta:
         db_table = 'user'
+    def __unicode__(self):
+        return self.name
 
